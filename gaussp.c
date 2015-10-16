ce25a5c779170fe80375d86637ff961724428d29
Obside <obsiagae@gmail.com>
2015-10-16 23:42:02 +0200

    gaussp

--- gaussp.c
+++ gaussp.c
@@ -15,12 +15,12 @@
  * Stores N / nTasks lines in the (partial) matrix parameter for T0.
  * Sends N / nTasks lines to each task. Each task can then receive and store the
  * lines in its partial matrix.
- * @Note This function must be executed by the task having the id 0 (T0) in the 
+ * @Note This function must be executed by the task having the id 0 (T0) in the
  * previously created pvm group.
  */
-void matrix_load(char filename[], double *matrix, int N, int nTasks) {	
-	FILE *f = fopen(filename, "r");
-    if (f == NULL) { perror("matrix_load : fopen "); } 
+void matrix_load(char filename[], double *matrix, int N, int nTasks) {
+    FILE *f = fopen(filename, "r");
+    if (f == NULL) { perror("matrix_load : fopen "); }
     
     double *lineBuffer = malloc(N * sizeof(double));
     
@@ -29,8 +29,8 @@ void matrix_load(char filename[], double *matrix, int N, int nTasks) {
         for(size_t j = 0 ; j < N ; j++) {
             fscanf(f, "%lf", &lineBuffer[j]);
         }
-		
-		int taskGrpId = i % nTasks;
+        
+        int taskGrpId = i % nTasks;
         if (taskGrpId == 0) {
             memcpy(&matrix[counter * N], lineBuffer, N * sizeof(double));
             counter++;
@@ -61,13 +61,13 @@ void partial_matrix_recv(double* matrix, int N, int nTasks) {
 /**
  * Sends a partial matrix to T0.
  */
-void partial_matrix_send(double* matrix, int N, int myGrpId, int nTasks) {
-	int dest = pvm_gettid(GRPNAME, 0);
-	for (size_t i = 0 ; i < N / nTasks ; i++) {
+void partial_matrix_send(double* matrix, int N, int nTasks) {
+    int dest = pvm_gettid(GRPNAME, 0);
+    for (size_t i = 0 ; i < N / nTasks ; i++) {
         pvm_initsend(PvmDataDefault);
         pvm_pkdouble(&matrix[i*N], N, 1);
         pvm_send(dest, 0);
-	}
+    }
 }
 
 /**
@@ -76,19 +76,17 @@ void partial_matrix_send(double* matrix, int N, int myGrpId, int nTasks) {
  * the whole matrix by receiving missing lines from the other tasks.
  * @note This function must be executed by T0.
  */
-void matrix_save(
-	char filename[], double *matrix, int N, int myGrpId, int nTasks
-) {    
+void matrix_save(char filename[], double *matrix, int N, int nTasks) {
     FILE *f = fopen(filename, "w");
     if(f == NULL) { perror("matrix_save : fopen "); }
     
-	double *lineBuffer = malloc(N * sizeof(double)); 
+    double *lineBuffer = malloc(N * sizeof(double));
     
     int counter = 0; // number of lines saved in the file for the task 0
     for(size_t i = 0 ; i < N ; i++) {
         if (i % nTasks == 0) { // T0 saves its line
             for(size_t j = 0 ; j < N ; j++) {
-            	fprintf(f, "%8.2f ", matrix[counter*N + j]);
+                fprintf(f, "%8.2f ", matrix[counter*N + j]);
             }
             fprintf(f, "\n");
             counter++;
@@ -111,39 +109,58 @@ void matrix_save(
 void matrix_display(double *matrix, int myGrpId, int N, int nTasks) {
     printf("myGrpId: %d\n", myGrpId);
     for (size_t i = 0 ; i < N/nTasks ; i++) {
-	    for (size_t j = 0 ; j < N ; j++) {
-        	printf("%f ", matrix[i*N + j]);
+        for (size_t j = 0 ; j < N ; j++) {
+            printf("%f ", matrix[i*N + j]);
         }
         printf("\n");
     }
     printf("\n");
 }
 
-void gauss(double* matrix, int N) {
-    double pivot;
-
-    for(size_t k = 0 ; k < N-1 ; k++) { // mise a 0 de la col. k
-        // printf(". ");
-        if (fabs(*(matrix+k+k*N)) <= 1.0e-11) {
-            printf("ATTENTION: pivot %zu presque nul: %g\n", k, *(matrix+k+k*N));
+void gauss(double* matrix, int myGrpId, int N, int nTasks) {
+    double *lineBuffer = malloc(N * sizeof(double));
+    
+    for (size_t k = 0 ; k < N - 1 ; k++) {
+        if (k % nTasks == myGrpId) {
+            memcpy(
+                &lineBuffer[k],
+                &matrix[(k/nTasks)*N + k],
+                (N - k) * sizeof(double)
+            );
+            pvm_initsend(PvmDataDefault);
+            pvm_pkdouble(&lineBuffer[k], N - k, 1);
+            pvm_bcast(GRPNAME, k);
+        }
+        else {
+            pvm_recv(-1, k);
+            pvm_upkdouble(&lineBuffer[k], N - k, 1);
+        }
+        
+        if (fabs(lineBuffer[k]) <= 1.0e-11) {
+            printf("ATTENTION: pivot %zu presque nul: %g\n", k, lineBuffer[k]);
             exit(EXIT_FAILURE);
         }
-        for(size_t i = k+1 ; i < N ; i++) { // update lines(k+1) to(n-1)
-            pivot = - *(matrix+k+i*N) / *(matrix+k+k*N);
-            for(size_t j = k ; j < N ; j++) { // update elts(k) -(N-1) of line i
-                *(matrix+j+i*N) = *(matrix+j+i*N) + pivot * *(matrix+j+k*N);
+        
+        size_t iStart = (k / nTasks);
+        if (myGrpId <= (k % nTasks)) { iStart++; }
+        
+        for (size_t i = iStart ; i < N/nTasks ; i++) {
+            double pivot = - matrix[i*N + k] / lineBuffer[k];
+            
+            for (size_t j = k ; j < N ; j++) {
+                matrix[i*N + j] += pivot * lineBuffer[j];
             }
-            // *(matrix+k+i*N) = 0.0;
         }
     }
-    printf("\n");
+    
+    free(lineBuffer);
 }
 
 void dowork(char filename[], int myGrpId, int N, int nTasks) {
     double *partialMatrix = malloc((N/nTasks) * N * sizeof(double));
-    if (partialMatrix == NULL) { 
-	    printf("Malloc failed\n");
-	    exit(EXIT_FAILURE);
+    if (partialMatrix == NULL) {
+        printf("Malloc failed\n");
+        exit(EXIT_FAILURE);
     }
     
     if (myGrpId == 0) {
@@ -155,14 +172,14 @@ void dowork(char filename[], int myGrpId, int N, int nTasks) {
     
     matrix_display(partialMatrix, myGrpId, N, nTasks);
     
-    // TODO compute gauss
-	
-	if (myGrpId == 0) {
-	    sprintf(filename+strlen(filename), ".result");
-    	matrix_save(filename, partialMatrix, N, myGrpId, nTasks);
+    gauss(partialMatrix, myGrpId, N, nTasks);
+    
+    if (myGrpId == 0) {
+        sprintf(filename+strlen(filename), ".result");
+        matrix_save(filename, partialMatrix, N, nTasks);
     }
     else {
-    	partial_matrix_send(partialMatrix, N, myGrpId, nTasks);
+        partial_matrix_send(partialMatrix, N, nTasks);
     }
     
     free(partialMatrix);
@@ -185,8 +202,8 @@ int main(int argc, char ** argv) {
     // determine the number of tasks
     int *tids;
     int nTasks = pvm_siblings(&tids);
-	
-	// join group & get current task's index in the group
+
+    // join group & get current task's index in the group
     int myGrpId = pvm_joingroup(GRPNAME);
     pvm_barrier(GRPNAME, nTasks);
     pvm_freezegroup(GRPNAME, nTasks);
